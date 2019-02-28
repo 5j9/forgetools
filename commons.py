378cf2ee813629363c871a85a798cf77ad3cf125
5j9 <5j9@users.noreply.github.com>
2019-02-28 10:49:04 +0330

    Send the stderr of webservice check to DEVNULL

--- commons.py
+++ commons.py
@@ -1,11 +1,12 @@
 from os.path import expanduser
-from subprocess import check_call, CalledProcessError
+from subprocess import check_call, CalledProcessError, DEVNULL
 
 HOME = expanduser('~')  # py34
 
 try:
-    check_call('webservice status', shell=True)
-except CalledProcessError:  # webservice is not available in Kubernetes pod
+    check_call('webservice status', shell=True, stderr=DEVNULL)
+except CalledProcessError:
+    print('Working in a Kubernetes container.')
     KUBERNETES = True
 else:
     KUBERNETES = False
