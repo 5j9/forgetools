cd7418a5459bcb8459025b4d8eb9eece3790e004
5j9 <5j9@users.noreply.github.com>
2019-03-02 13:54:06 +0330

    Add kub_python_update.py

--- commons.py
+++ commons.py
@@ -1,11 +1,13 @@
 from os.path import expanduser
-from subprocess import check_call, CalledProcessError, DEVNULL
+from subprocess import check_output, CalledProcessError
 
 HOME = expanduser('~')  # py34
 
 try:
-    check_call('kubectl get pods', shell=True)
+    POD_NAME = check_output('kubectl get pod -o name', shell=True)
 except CalledProcessError:
     KUBERNETES = True
+    POD_NAME = None
 else:
     KUBERNETES = False
+    POD_NAME = POD_NAME[4:].rstrip().decode()
---
+++ kub_python_update.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+from subprocess import check_call, check_output
+from os import chdir, remove
+from commons import HOME, POD_NAME
+
+
+def pull_updates():
+    chdir(HOME + '/www/python/src')
+    check_call('git reset --hard && git pull', shell=True)
+
+
+def install_requirements():
+    upgrade_commands = (
+        '. ~/www/python/venv/bin/activate '
+        '&& pip install --upgrade pip '
+        '&& pip install -Ur ~/www/python/src/requirements.txt'
+    )
+    if POD_NAME:
+        check_output(
+            'kubectl exec '
+            + POD_NAME
+            + ' -- bash -c "' + upgrade_commands + '"',
+            shell=True
+        )
+        return
+    # already in pod's shell
+    check_call(upgrade_commands, shell=True)
+
+
+def rm_old_files():
+    try:
+        remove(HOME + '/uwsgi.log')
+    except FileNotFoundError:
+        pass
+    try:
+        remove(HOME + '/error.log')
+    except FileNotFoundError:
+        pass
+
+
+def main():
+    rm_old_files()
+    pull_updates()
+    install_requirements()
+    check_output(
+        'webservice --backend=kubernetes python restart', shell=True)
+
+
+if __name__ == '__main__':
+    main()
