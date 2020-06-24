349cce22b0a997c40818f11ba1cb07db9399622e
5j9 <5j9@users.noreply.github.com>
2020-06-24 13:36:22 +0430

    feat(install_python.py): support alpha and beta versions

--- install_python.py
+++ install_python.py
@@ -6,6 +6,7 @@ otherwise it'll be in ~/pythons/ve{num_ver}.
 """
 
 from argparse import ArgumentParser
+from distutils.version import StrictVersion
 from io import BytesIO
 from os import mkdir, chdir, getcwd
 from shutil import rmtree
@@ -23,52 +24,44 @@ from commons import HOME, KUBERNETES
 PYTHONS = HOME + '/pythons'
 
 
-def download_info(num_ver=None) -> tuple:
-    """Return (url, num_ver, dot_ver).
-
-    Return the latest version info if num_ver is None.
-    """
-    if num_ver:
-        dot_ver = '.'.join(num_ver)
-        return (
-            'https://www.python.org/ftp/python/{dot_ver}/'
-            'Python-{dot_ver}.tar.xz'.format(dot_ver=dot_ver),
-            num_ver,
-            dot_ver)
+def download_info(ver=None) -> tuple:
+    """Return the url of the latest version if num_ver is None."""
+    if ver:
+        main_ver = '.'.join(str(v) for v in StrictVersion(ver).version)
+        return ver, (
+            'https://www.python.org/ftp/python/{main_ver}/'
+            'Python-{full_ver}.tar.xz'.format(main_ver=main_ver, full_ver=ver))
     downloads = urlopen('https://www.python.org/downloads/').read()
-    m = search(
+    g = search(
         rb'href="(?P<url>https://www\.python\.org/ftp/python/'
         rb'(?P<dot_ver>\d+\.\d+\.\d+)/Python-(?P=dot_ver)\.tar\.xz)"',
-        downloads)
-    dot_ver = m.group('dot_ver').decode()
-    return (
-        m.group('url').decode(),
-        dot_ver.replace('.', ''),
-        dot_ver)
+        downloads).group
+    return g('dot_ver').decode(), g('url').decode()
 
 
-def download_python(num_ver=None) -> tuple:
-    """Download installer, extract it, return the installer dir and num_ver."""
-    url, num_ver, dot_ver = download_info(num_ver)
-    source_path = PYTHONS + '/Python-' + dot_ver
+def download_python(ver=None) -> str:
+    """Download installer, extract it, return the installer dir."""
+    ver, url = download_info(ver)
+    source_path = PYTHONS + '/Python-' + ver
+    print(url)
     tar_file = tarfile_open(fileobj=BytesIO(urlopen(url).read()))
     tar_file.extractall(PYTHONS)
-    return num_ver, source_path
+    return source_path
 
 
-def install_python(source_path, num_ver) -> None:
+def install_python(source_path, ver) -> None:
     """Install the requested python version."""
     owd = getcwd()
     chdir(source_path)
-    check_call('./configure --prefix=' + PYTHONS + '/' + num_ver, shell=True)
+    check_call('./configure --prefix=' + PYTHONS + '/' + ver, shell=True)
     check_call('make  --directory=' + source_path, shell=True)
     check_call('make install --directory=' + source_path, shell=True)
     chdir(owd)
 
 
-def setup_vitual_env(num_ver, requirements=None):
+def setup_vitual_env(ver, requirements=None):
     """Create a virtual env using {num_ver}/bin/python3 in ve{num_ver} dir."""
-    python3 = PYTHONS + '/' + num_ver + '/bin/python3'
+    python3 = PYTHONS + '/' + ver + '/bin/python3'
     if KUBERNETES:
         venv = HOME + '/www/python/venv'
         try:
@@ -79,7 +72,7 @@ def setup_vitual_env(num_ver, requirements=None):
             pass
         check_call(python3 + ' -m venv ' + venv, shell=True)
         return
-    ve_path = PYTHONS + '/ve' + num_ver
+    ve_path = PYTHONS + '/ve' + ver
     check_call(python3 + ' -m venv ' + ve_path, shell=True)
     if requirements:
         # Activate the newly created environment and install packages
@@ -90,14 +83,14 @@ def setup_vitual_env(num_ver, requirements=None):
         check_call(script, shell=True)
 
 
-def main(num_ver=None):
+def main(ver=None):
     try:
         mkdir(PYTHONS)
     except FileExistsError:
         pass
-    num_ver, source_path = download_python(num_ver)
-    install_python(source_path, num_ver)
-    setup_vitual_env(num_ver)
+    source_path = download_python(ver)
+    install_python(source_path, ver)
+    setup_vitual_env(ver)
     create_profile()
 
 
@@ -108,6 +101,7 @@ if __name__ == '__main__':
                     'Can be overridden by giving a specific Python version as '
                     'a 3-digit number.')
     arg_parser.add_argument(
-        'pyver', nargs='?', help='The desired Python version, e.g. 372.')
+        'pyver', nargs='?',
+        help='The desired Python version, e.g. 3.7.2 or 3.9.0b3.')
     args = arg_parser.parse_args()
     main(args.pyver)
