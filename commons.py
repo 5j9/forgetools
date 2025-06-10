f3dfe52008fa27f32044a59adb8155baa8b52e60
5j9 <5j9@users.noreply.github.com>
2025-06-10 18:08:38 +0330

    fix(get_pod_name): verbose_run args are str

--- commons.py
+++ commons.py
@@ -48,7 +48,7 @@ def assert_webservice_control(script_name):
 
 def get_pod_name():
     try:
-        pod_name = verbose_run(('kubectl', 'get', 'pod', '-o', 'name')).stdout
+        pod_name = verbose_run('kubectl', 'get', 'pod', '-o', 'name').stdout
     except CalledProcessError:
         return None
     return pod_name[4:].splitlines()[-1].decode()
